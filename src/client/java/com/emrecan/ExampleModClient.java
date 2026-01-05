bff411ae2c4e9d62c83cd0ddfefdbd8b606ec400
Emrecana24 <150844555+Emrecana24@users.noreply.github.com>
2026-01-05 08:56:40 +0000

    Mod baslangic hali - Java ayarlari ve proje duzeni

---
+++ src/client/java/com/emrecan/ExampleModClient.java
@@ -0,0 +1,10 @@
+package com.emrecan;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-only code (render, model vs)
+    }
+}
