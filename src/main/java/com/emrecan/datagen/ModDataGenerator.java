48825506968f6313b87fcc0852bc43f40a46503e
Emrecana24 <150844555+Emrecana24@users.noreply.github.com>
2026-01-05 22:54:38 +0000

    son hali

---
+++ src/main/java/com/emrecan/datagen/ModDataGenerator.java
@@ -0,0 +1,12 @@
+package com.emrecan.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        generator.addProvider(ModLootTableProvider::new);
+    }
+}
